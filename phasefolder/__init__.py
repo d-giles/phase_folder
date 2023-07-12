4381f5c820b24413f06c0581648738775f0af29c
Daniel Giles <dgiles@seti.org>
2023-07-12 23:00:01 +0000

    fixed import

--- phasefolder/__init__.py
+++ phasefolder/__init__.py
@@ -3,5 +3,4 @@
 __author__ = """Daniel Giles"""
 __email__ = 'daniel.k.giles@gmail.com'
 __version__ = '0.1.0'
-from .phasefolder import find_period
-from .notebook_interact import notebook_interact
+from .phasefolder import find_period, notebook_interact
